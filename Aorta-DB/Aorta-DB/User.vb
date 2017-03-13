1f0dc7c50f7c12dc93fe073c01fe8f775f6c6eb8
torsteinbl <torstein_bl@hotmail.com>
2017-03-13 11:37:06 +0100

    completed the user constructor

--- Aorta-DB/Aorta-DB/User.vb
+++ Aorta-DB/Aorta-DB/User.vb
@@ -3,7 +3,12 @@
     Private type As Integer
     Private fornavn As String
     Private etternavn As String
-    Sub New(personnummer As Decimal)
-        personnr = personnummer
+    Private list As New ArrayList
+    Public Sub New(ByVal personnummer As Decimal)
+        list = return_user(personnummer)
+        personnr = list(0)
+        type = list(4)
+        fornavn = list(2)
+        etternavn = list(3)
     End Sub
 End Class
