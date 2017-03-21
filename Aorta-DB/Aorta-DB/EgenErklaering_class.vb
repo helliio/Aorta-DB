5c21956a20359ec111c2e72e19e68c06f3da4062
Liang Zhu <liang150393@gmail.com>
2017-03-21 10:58:07 +0100

    add egenerklaring class

---
+++ Aorta-DB/Aorta-DB/EgenErklaering_class.vb
@@ -0,0 +1,19 @@
+﻿Public Class EgenErklaering_class
+    Private answer(59) As Boolean
+    Private pers As Decimal
+    Public Sub New(ByVal personnummer As Decimal)
+        pers = personnummer
+        For index As Integer = 0 To 59
+            answer(index) = False
+        Next
+    End Sub
+    Public Sub setAns(i As Integer)
+        answer(i) = True
+    End Sub
+    Public Function getAns(i As Integer)
+        Return answer(i)
+    End Function
+    Public Sub send()
+
+    End Sub
+End Class
