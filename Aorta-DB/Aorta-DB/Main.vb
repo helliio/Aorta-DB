76d4fd5ca6d1a67f29c9a1e5e79c85a8f3f3e8da
Liang Zhu <liang150393@gmail.com>
2017-03-22 02:53:54 +0100

    removed debug button

--- Aorta-DB/Aorta-DB/Main.vb
+++ Aorta-DB/Aorta-DB/Main.vb
@@ -9,10 +9,4 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnEgenerklæring.Click
         EgenerklaeringLoggInn.Show()
     End Sub
-
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
-        BlodGivning.Show()
-    End Sub
-
-
 End Class
