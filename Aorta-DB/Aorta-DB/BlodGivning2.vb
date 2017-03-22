ca270fa49d00fd5350f571290f21f3fc154b3bde
Liang Zhu <liang150393@gmail.com>
2017-03-22 03:16:37 +0100

    add blod pakke

--- Aorta-DB/Aorta-DB/BlodGivning2.vb
+++ Aorta-DB/Aorta-DB/BlodGivning2.vb
@@ -1,10 +1,20 @@
 ﻿Public Class BlodGivning2
     Private Sub BlodGivning2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ComboBox1.Items.Add("")
-        ComboBox1.Items.Add("")
-        ComboBox1.Items.Add("")
-        ComboBox1.Items.Add("")
-        ComboBox1.Items.Add("")
-        ComboBox1.Items.Add("")
+        ComboBox1.Items.Add("O+")
+        ComboBox1.Items.Add("O-")
+        ComboBox1.Items.Add("A+")
+        ComboBox1.Items.Add("A-")
+        ComboBox1.Items.Add("B+")
+        ComboBox1.Items.Add("B-")
+        ComboBox1.Items.Add("AB+")
+        ComboBox1.Items.Add("AB-")
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If ComboBox1.Text = "Blodtype" Then
+            MsgBox("velg type")
+        Else
+
+        End If
     End Sub
 End Class
---
+++ Aorta-DB/Aorta-DB/Blod_pakke.vb
@@ -0,0 +1,10 @@
+﻿Public Class Blod_pakke
+    Private persnr As Decimal
+    Private dato As String
+    Private type As String
+    Public Sub New(ByVal personnummer As Decimal, ByVal type As String)
+        persnr = personnummer
+        Me.type = type
+        dato = erklaring.getDate()
+    End Sub
+End Class
