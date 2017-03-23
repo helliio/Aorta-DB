131af0468605228b25f28625f27ae91a5066fbe5
henrikrox <hroksvaag@hotmail.com>
2017-03-23 12:54:29 +0100

    main design

--- Aorta-DB/Aorta-DB/Main.vb
+++ Aorta-DB/Aorta-DB/Main.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Main
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnLoggInn.Click
         Logginn.Show()
@@ -10,5 +11,13 @@
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnEgenerklæring.Click
         EgenerklaeringLoggInn.Show()
         Me.Hide()
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
     End Sub
 End Class
