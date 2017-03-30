2f6802935e9f83b49b9cda1a4beb2a5de0d99a83
henrikrox <hroksvaag@hotmail.com>
2017-03-30 20:54:41 +0200

    gui main, brukerhovedside,logg inn

--- Aorta-DB/Aorta-DB/Main.vb
+++ Aorta-DB/Aorta-DB/Main.vb
@@ -13,7 +13,9 @@
         Me.Hide()
     End Sub
 
-    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
 
+    Private Sub btnNyBruker_Click(sender As Object, e As EventArgs) Handles btnNyBruker.Click
+        NyBruker.Show()
+        Me.Hide()
     End Sub
 End Class
