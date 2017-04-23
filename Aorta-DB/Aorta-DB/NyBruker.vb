da675cab65ba7b557b1063721db60e5c510b2b42
torsteinbl <torstein_bl@hotmail.com>
2017-04-23 12:48:14 +0200

    fix

--- Aorta-DB/Aorta-DB/NyBruker.vb
+++ Aorta-DB/Aorta-DB/NyBruker.vb
@@ -24,26 +24,26 @@ Public Class NyBruker
             persnr = Convert.ToDecimal(txtPersonnummer.Text)
         Else
             'some error
-            MsgBox("invalid personnummer")
+            MsgBox("Ugyldig personnummer")
             Exit Sub
         End If
         If IsNumeric(txtTelefon.Text) And txtTelefon.Text.Length >= 8 Then
             tlf = Convert.ToDecimal(txtTelefon.Text)
         Else
             'some error
-            MsgBox("invalid telefonnummer")
+            MsgBox("Ugyldig telefonnummer")
             Exit Sub
         End If
         If Not validateEmail(txtMail.Text) Then
             'some error
-            MsgBox("invalid email")
+            MsgBox("Ugyldig email")
             Exit Sub
         End If
         If IsNumeric(txtPostnummer.Text) And txtPostnummer.Text.Length = 4 Then
             postnr = CInt(txtPostnummer.Text)
         Else
             'some error
-            MsgBox("invalid postnummer")
+            MsgBox("Ugyldig postnummer")
             Exit Sub
         End If
         If Not txtPassord.Text.Length >= 8 Then
@@ -70,11 +70,6 @@ Public Class NyBruker
     Private Sub NyBruker_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
         Me.Hide()
     End Sub
-    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
-        Logginn.Show()
-        Me.Hide()
-    End Sub
-
     Private Sub logo_Click(sender As Object, e As EventArgs) Handles logo.Click
         Main.Show()
         Me.Close()
@@ -87,5 +82,10 @@ Public Class NyBruker
 
     Private Sub btnKontakt_Click(sender As Object, e As EventArgs) Handles btnKontakt.Click
         MsgBox("Tlf: 12345678" & vbNewLine & "Epost: kontakt@aorta.db")
+    End Sub
+
+    Private Sub btnLoggInn_Click(sender As Object, e As EventArgs) Handles btnLoggInn.Click
+        Logginn.Show()
+        Me.Close()
     End Sub
 End Class
